131fe3206f949619043334ab827719471c66e124
amirrezaask <raskarpour@gmail.com>
2020-09-26 15:26:48 +0330

    add redis as state backend

--- state/redis.go
+++ state/redis.go
@@ -1,11 +1,46 @@
 package state
 
-import "github.com/amirrezaask/plumber"
+import (
+	"context"
+	"fmt"
+	"time"
 
-type RedisState struct {
-	conn
+	"github.com/amirrezaask/plumber"
+	"github.com/go-redis/redis/v8"
+)
+
+type redisState struct {
+	ctx  context.Context
+	conn *redis.Client
+	ttl  time.Duration
 }
 
-func NewRedisState(host, port, user, password, database string) plumber.State {
+func NewRedisState(ctx context.Context, host, port, user, password string, database int) plumber.State {
 
+	c := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", host, port),
+		DB:       database,
+		Username: user,
+		Password: password,
+	})
+	return &redisState{
+		conn: c,
+		ctx:  ctx,
+	}
+}
+
+func (r *redisState) Get(key string) (interface{}, error) {
+	res := r.conn.Get(r.ctx, key)
+	if err := res.Err(); err != nil {
+		return nil, err
+	}
+	return res.Val(), nil
+}
+
+func (r *redisState) Set(key string, value interface{}) error {
+	statusCmd := r.conn.Set(r.ctx, key, value, r.ttl)
+	if err := statusCmd.Err(); err != nil {
+		return err
+	}
+	return nil
 }
